7268a4895dbade6da9fa0ce082afb0fd41c2f400
Andres181 <30846341+Andres181@users.noreply.github.com>
2017-09-26 22:25:07 -0500

    Add files via upload

---
+++ Expresiones.py
@@ -0,0 +1,36 @@
+import re
+
+cadena = "A F M 5 # 1 / % * 5 R 5"
+lista = cadena.split(" ")
+patronVariables = re.compile('[A-Z]')
+patronValores = re.compile('[0-9]')
+patronSignos = re.compile('[+*/=-]')
+
+listaErrores1 = [x for x in lista if x not in patronVariables.findall(cadena)]
+listaErrores2 = [x for x in listaErrores1 if x not in patronValores.findall(cadena)]
+listaErroresFinal = [x for x in listaErrores2 if x not in patronSignos.findall(cadena)]
+
+if len(listaErroresFinal)==0:
+    print "No hay errores."
+else:
+    print "Errores existentes: "
+    print len(listaErroresFinal)
+    print listaErroresFinal
+    print "\n\n\n"
+    
+cadenaSalida = ""
+
+for i in range (len(lista)):
+    if not(patronVariables.search(lista[i]) is None):
+        cadenaSalida = cadenaSalida + "("+lista[i]+",VARIABLE)\n"
+
+    elif not(patronValores.search(lista[i]) is None):
+        cadenaSalida = cadenaSalida + "("+lista[i]+",VALOR)\n"
+        
+    elif not(patronSignos.search(lista[i]) is None):
+        cadenaSalida = cadenaSalida + "("+lista[i]+",SIGNO)\n"
+
+    else:
+        cadenaSalida = cadenaSalida + "("+lista[i]+",ERROR)\n"
+
+print cadenaSalida
