a5686d37dc582d3581c2cc1a475e41489414a6df
Andres181 <30846341+Andres181@users.noreply.github.com>
2017-10-10 10:52:51 -0500

    Update Expresiones.py

--- Expresiones.py
+++ Expresiones.py
@@ -1,6 +1,6 @@
 import re
 
-Cadena =raw_input("Ingrese la operacion en posorden: ")
+cadena =raw_input("Ingrese la operacion en posorden: ")
 lista = cadena.split(" ")
 patronVariables = re.compile('[A-Z]', re.I)
 patronValores = re.compile('[0-9]')
@@ -10,20 +10,27 @@ listaErrores1 = [x for x in lista if x not in patronVariables.findall(cadena)]
 listaErrores2 = [x for x in listaErrores1 if x not in patronValores.findall(cadena)]
 listaErroresFinal = [x for x in listaErrores2 if x not in patronSignos.findall(cadena)]
 
+for j in listaErroresFinal:
+    lista.remove(j)
+
+
 if len(listaErroresFinal)==0:
     print "No hay errores."
+
+    
 else:
     print "Errores existentes: "
     print len(listaErroresFinal)
     print listaErroresFinal
     print "\n\n\n"
     
+
 cadenaSalida = ""
 
 for i in range (len(lista)):
     if not(patronVariables.search(lista[i]) is None):
         cadenaSalida = cadenaSalida + "("+lista[i]+",VARIABLE)\n"
-
+    
     elif not(patronValores.search(lista[i]) is None):
         cadenaSalida = cadenaSalida + "("+lista[i]+",VALOR)\n"
         
